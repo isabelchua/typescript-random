422b731d3bcb9607c50a8757f34989a80330aa18
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-20 10:42:36 -0700

    finished class

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = 'Welcome Back';
 console.log(message);
@@ -24,3 +37,102 @@ var Color;
 })(Color || (Color = {}));
 var c = Color.Green;
 console.log(c);
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Isabel';
+//let myVariable: any = 10;
+var myVariable = 10;
+//check if name property exists in obj or not
+function hasName(obj) {
+    return !!obj && typeof obj === 'object' && 'name' in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+//console.log(myVariable.name);
+//myVariable();
+//(myVariable as string).toUpperCase();
+var a;
+a = 10;
+a = true;
+var b = 20;
+//b = true;
+//intellisense support
+//b.
+var multiType;
+multiType = 20;
+multiType = true;
+//intellisense support with union type
+var anyType;
+anyType = 20;
+//using types with functions
+function add(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+console.log(add(5, 10));
+// ? is optional undefined
+function add1(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+console.log(add1(5, 10));
+// add(5, '10')
+// add()
+function add2(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+console.log(add2(5, 10));
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayve'
+};
+fullName(p);
+function fullName2(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p2 = {
+    firstName: 'Bruce'
+    //	lastName: 'Wayve'
+};
+//fullName(p2);
+//public free accessibility
+//private accessibility within the class
+//protected within the class and classes derived from it
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Isabel');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating task");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -52,4 +52,114 @@ if (hasName(myVariable)) {
 
 //console.log(myVariable.name);
 //myVariable();
-(myVariable as string).toUpperCase();
+//(myVariable as string).toUpperCase();
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+//b = true;
+//intellisense support
+//b.
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+//intellisense support with union type
+
+let anyType: any;
+anyType = 20;
+
+//using types with functions
+function add(num1: number, num2: number): number {
+	if (num2) return num1 + num2;
+	else return num1;
+}
+console.log(add(5, 10));
+
+// ? is optional undefined
+function add1(num1: number, num2?: number): number {
+	if (num2) return num1 + num2;
+	else return num1;
+}
+console.log(add1(5, 10));
+// add(5, '10')
+// add()
+
+function add2(num1: number, num2: number = 10): number {
+	if (num2) return num1 + num2;
+	else return num1;
+}
+console.log(add2(5, 10));
+
+function fullName(person: { firstName: string; lastName: string }) {
+	console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p = {
+	firstName: 'Bruce',
+	lastName: 'Wayve'
+};
+
+fullName(p);
+
+//easier to see more nice to see
+
+//adding ? makes it an optional type
+
+// but throws and error? not sure why ):
+
+interface Person2 {
+	firstName: string;
+	lastName?: string;
+}
+
+function fullName2(person: Person2) {
+	console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p2 = {
+	firstName: 'Bruce'
+	//	lastName: 'Wayve'
+};
+
+//fullName(p2);
+
+//public free accessibility
+//private accessibility within the class
+//protected within the class and classes derived from it
+class Employee {
+	//private employeeName: string;
+	//public employeeName: string;
+	//protected employeeName: string;
+
+	employeeName: string;
+
+	constructor(name: string) {
+		this.employeeName = name;
+	}
+
+	greet() {
+		console.log(`Good Morning ${this.employeeName}`);
+	}
+}
+
+let emp1 = new Employee('Isabel');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+	constructor(managerName: string) {
+		super(managerName);
+	}
+	delegateWork() {
+		console.log(`Manager delegating task`);
+	}
+}
+
+let m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
